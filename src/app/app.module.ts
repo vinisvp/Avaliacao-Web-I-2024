38a919fd0534b5e10f66c6b625c8cda166c510b1
vinisvp <viniciuspedro0905@gmail.com>
2024-06-18 14:19:08 -0300

    Added the main components of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TypesComponent } from './features/types/types.component';
+import { ContactsComponent } from './features/contacts/contacts.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypesComponent,
+    ContactsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/features/types/types.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-types',
+  templateUrl: './types.component.html',
+  styleUrl: './types.component.css'
+})
+export class TypesComponent {
+
+}
