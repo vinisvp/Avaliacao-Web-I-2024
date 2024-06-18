a88b2e09cd2e294367e6dbf54b7b6708a73e50b1
vinisvp <viniciuspedro0905@gmail.com>
2024-06-18 14:58:52 -0300

    Added list of types in a table

--- src/app/features/types/types.component.ts
+++ src/app/features/types/types.component.ts
@@ -1,14 +1,16 @@
 import { TypesService } from './../../services/types.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Type } from './type';
 
 @Component({
   selector: 'app-types',
   templateUrl: './types.component.html',
   styleUrl: './types.component.css'
 })
-export class TypesComponent {
+export class TypesComponent implements OnInit {
   typesFormGroup: FormGroup;
+  types: Type[] = [];
 
   constructor(private formBuilder: FormBuilder,
               private typesService: TypesService
@@ -19,8 +21,19 @@ export class TypesComponent {
     })
   }
 
+  loadTypes(){
+    this.typesService.getType().subscribe({
+      next: data => this.types = data
+    })
+  }
+
+  ngOnInit(): void {
+    this.loadTypes();
+  }
+
   save(){
     this.typesService.postType(this.typesFormGroup.value).subscribe({
+      next: () => this.loadTypes()
     })
   }
 }
