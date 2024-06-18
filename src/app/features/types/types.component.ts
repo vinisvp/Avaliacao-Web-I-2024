2262587db983ada720ea67204d7369d0f5203abd
vinisvp <viniciuspedro0905@gmail.com>
2024-06-18 14:49:54 -0300

    Added a save type function and some template for the types component

--- src/app/features/types/types.component.ts
+++ src/app/features/types/types.component.ts
@@ -1,4 +1,6 @@
+import { TypesService } from './../../services/types.service';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-types',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './types.component.css'
 })
 export class TypesComponent {
+  typesFormGroup: FormGroup;
 
+  constructor(private formBuilder: FormBuilder,
+              private typesService: TypesService
+  ){
+    this.typesFormGroup = formBuilder.group({
+     id:[''],
+     name:['']
+    })
+  }
+
+  save(){
+    this.typesService.postType(this.typesFormGroup.value).subscribe({
+    })
+  }
 }
---
+++ src/app/services/types.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Type } from '../features/types/type';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TypesService {
+  url = 'http://localhost:3000/type/';
+
+  constructor(private http: HttpClient) { }
+
+  postType(type: Type): Observable<Type>{
+    return this.http.post<Type>(this.url, type);
+  }
+
+  getType(): Observable<Type[]>{
+    return this.http.get<Type[]>(this.url);
+  }
+
+  putType(type: Type): Observable<Type>{
+    return this.http.put<Type>(`${this.url}/${type.id}`, type);
+  }
+}
