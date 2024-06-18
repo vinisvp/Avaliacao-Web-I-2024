1e3c6915a8e4811e459a510b21426a7959d11e26
vinisvp <viniciuspedro0905@gmail.com>
2024-06-18 14:23:19 -0300

    Added the HttpClient and the ReactiveForms Module, and set the paths of the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './features/contacts/contacts.component';
+import { TypesComponent } from './features/types/types.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'contacts', component: ContactsComponent},
+  {path: 'types', component: TypesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TypesComponent } from './features/types/types.component';
 import { ContactsComponent } from './features/contacts/contacts.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { NavbarComponent } from './core/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
